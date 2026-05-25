b2b8f508159ad7bfd6ee13a1660f6a3e038dab0f
Kresereb <65852286+Kresereb@users.noreply.github.com>
2026-05-25 15:57:23 +0200

    Add mobile menu script

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,52 @@
-// Site JS placeholder
+document.documentElement.classList.add("js");
+
+(function () {
+  const menuToggle = document.querySelector("[data-menu-toggle]");
+  const siteMenu = document.querySelector("[data-site-menu]");
+
+  if (!menuToggle || !siteMenu) {
+    return;
+  }
+
+  function openMenu() {
+    siteMenu.classList.add("is-open");
+    menuToggle.setAttribute("aria-expanded", "true");
+  }
+
+  function closeMenu() {
+    siteMenu.classList.remove("is-open");
+    menuToggle.setAttribute("aria-expanded", "false");
+  }
+
+  function toggleMenu() {
+    const isOpen = menuToggle.getAttribute("aria-expanded") === "true";
+
+    if (isOpen) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  }
+
+  menuToggle.addEventListener("click", toggleMenu);
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeMenu();
+    }
+  });
+
+  const desktopQuery = window.matchMedia("(min-width: 48rem)");
+
+  function handleViewportChange(event) {
+    if (event.matches) {
+      closeMenu();
+    }
+  }
+
+  if (desktopQuery.addEventListener) {
+    desktopQuery.addEventListener("change", handleViewportChange);
+  } else if (desktopQuery.addListener) {
+    desktopQuery.addListener(handleViewportChange);
+  }
+})();
